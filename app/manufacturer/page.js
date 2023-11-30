0b7534520f6f9d566f7e9684abca647ce51b2768
Soham <sohamborkar14@gmail.com>
2023-12-01 03:14:55 +0530

    added manufacturer page

---
+++ app/manufacturer/page.js
@@ -0,0 +1,42 @@
+'use client'
+
+import Link from "next/link"
+
+export default function AddProduct() {
+  return (
+    <div>
+        <section class="bg-gray-50 dark:bg-gray-900">
+        <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+          <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+            <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+              <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                Add Product
+              </h1>
+              <form class="space-y-4 md:space-y-6" action="#">
+                <div>
+                  <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Product Name</label>
+                  <input type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter product name" required=""/>
+                </div>
+                <div>
+                  <label for="id" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Product Id</label>
+                  <input type="number" name="id" id="id" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter product id" required=""/>
+                </div>
+                <div>
+                  <label for="batch" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Batch No.</label>
+                  <input type="number" name="batch" id="batch" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter product batch number" required=""/>
+                </div>
+                <div>
+                  <label for="price" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Product Price</label>
+                  <input type="number" name="price" id="price" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter product price" required=""/>
+                </div>
+                <div></div>
+                <button type="submit" class="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add Product</button>
+               
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  )
+}
